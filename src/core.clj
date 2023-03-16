920c80437b1d673cde6e10c9c8f3d956733fe9bd
Sven Dibowski <s.dibowski@intershop.de>
2023-03-16 14:03:01 +0100

    refac: some enhancements

--- src/core.clj
+++ src/core.clj
@@ -9,20 +9,16 @@
        :body
        (json/read-str :key-fn keyword)))
 
-(deftest successful-request-test
+(deftest successful-request-tests
   (testing "Request is not empty"
-    (is (not-empty (send-request) ))
-    (is (= 6 (count (send-request)))))) 
-
-((deftest compare-user-test
-   (testing "Single User"
-     (let [expected {:id 1,
-                     :email "george.bluth@reqres.in",
-                     :first_name "George",
-                     :last_name "Bluth",
-                     :avatar "https://reqres.in/img/faces/1-image.jpg"
-                     }
-           actual (first ((send-request) :data))]
-       (is (= expected actual) "returned map should be equals expacted one")))))
-
-
+    (let [request (send-request)]
+      (is (not-empty request))
+      (is (= 6 (count request)))))
+  (testing "Single User"
+    (let [expected {:id 1,
+                    :email "george.bluth@reqres.in",
+                    :first_name "George",
+                    :last_name "Bluth",
+                    :avatar "https://reqres.in/img/faces/1-image.jpg"}
+          actual (first ((send-request) :data))]
+      (is (= expected actual) "returned map should be equals expacted one"))))
